e1c03253a01e0f277eebf1611b4832b18f791d9f
Okky Maheswara <okkymahes@gmail.com>
2019-12-30 11:38:28 +0800

    merubah isian pd file koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,11 +1,14 @@
 <?php
+
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_indeks";
+$username = "indeksdp_user_indeks";
+$password = "!s7Jl3856+zM";
+$dbname = "indeksdp_db_indeks";
+
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
