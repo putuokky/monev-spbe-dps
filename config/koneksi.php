73883cd00cf0d231948fe561d6bba39482c8baf9
Okky Maheswara <okkymahes@gmail.com>
2019-12-30 13:06:31 +0800

    tess

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,14 +1,10 @@
 <?php
-
 $servername = "localhost";
 $username = "indeksdp_user_indeks";
 $password = "!s7Jl3856+zM";
 $dbname = "indeksdp_db_indeks";
-
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
