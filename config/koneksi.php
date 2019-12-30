a8b8894c541fe9a23480f5e6c41082c40860ac0d
Okky Maheswara <okkymahes@gmail.com>
2019-12-30 13:18:31 +0800

    merubah isi file koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "indeksdp_user_indeks";
-$password = "!s7Jl3856+zM";
-$dbname = "indeksdp_db_indeks";
+$username = "root";
+$password = "";
+$dbname = "db_indeks";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
