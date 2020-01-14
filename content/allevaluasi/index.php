84436cc960d3d29196a92ef5b4f71b637df0d74a
Okky Maheswara <okkymahes@gmail.com>
2020-01-14 08:19:34 +0800

    alala

--- content/allevaluasi/index.php
+++ content/allevaluasi/index.php
@@ -44,85 +44,51 @@ if (isset($_GET['act']) && $_GET['act'] == "hapus") {
             <div class="accordion" id="accordionExample">
             <?php
               $var = 0;
-              $sql = "SELECT * FROM tb_eksekutif_opd
-                      WHERE opd_terkait = $_SESSION[opd]";
+              $sql = "SELECT * FROM tb_eksekutif_opd a 
+                    LEFT JOIN tb_indikator b ON b.idindikator = a.indikator 
+                    WHERE a.opd_terkait = $_SESSION[opd] && a.tahun_eksekutif_opd = $_POST[thnevaluasi]";
               $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
+              if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $id = $row['id_eksekutif_opd'];
+                  $indikator = $row['indikator'];
+                  $nilai_pusat = $row['nilai_pusat'];
+                  $tahap_opd = explode(";", $row['tahapan_yg_harus_dipenuhi_opd']);
+                  $telah_miliki = $row['telah_miliki'];
+                  $belum_miliki = $row['belum_miliki'];
+                  $opd_terkait = $row['opd_terkait'];
+                  $tahun_eksekutif_opd = $row['tahun_eksekutif_opd'];
             ?>
-              <div class="card">
-                <div class="card-header" id="headingOne">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse<?php echo $var; ?>">
-                      <?= "Indikator ". $row['indikator']; ?>
-                    </button>
-                  </h2>
-                </div>
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse<?= $var; ?>">
+                        <?= "Indikator ". $row['indikator']; ?>
+                      </button>
+                    </h2>
+                  </div>
 
-                <div id="collapse<?php echo $var; ?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                  <div class="card-body">
-                    <div class="table-responsive">
-                      <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
-                        <thead align="center" >
-                          <tr>
-                            <th rowspan="2">No</th>
-                            <th rowspan="2">Nilai Pusat</th>
-                            <th rowspan="2">Tahapan Yang Harus dipenuhi OPD untuk menaikkan nilai SPBE</th>
-                            <th rowspan="2">OPD Terkait</th>
-                            <th colspan="2">Data Pendukung</th>
-                          </tr>
-                          <tr>
-                            <th>Telah dimiliki</th>
-                            <th>Belum dimiliki</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <?php
-                            $sqlDua = "SELECT * FROM tb_penilaian a 
-                            LEFT JOIN tb_indikator b ON b.idindikator = a.idindikator 
-                            LEFT JOIN tb_level c ON c.idlevel = a.penilaianmandiri 
-                            LEFT JOIN tb_opdterkait d ON d.idpenilaian = a.idpenilaian 
-                            LEFT JOIN tb_opd e ON e.idopd = d.idopd
-                            WHERE a.idindikator = $row[idindikator]";
-                            $resultDua = mysqli_query($conn, $sqlDua);
-
-                            if (mysqli_num_rows($resultDua) > 0) {
-                              $no = 1;
-                              // output data of each row
-                              while ($rowDua = mysqli_fetch_assoc($resultDua)) {
-                                $id = $rowDua['idpenilaian'];
-                                $tahun_penilaian = $rowDua['tahun_penilaian'];
-                                $nilaikematangan = $rowDua['nilaikematangan'];
-                                $penilaianmandiri = $rowDua['penilaianmandiri'];
-                                $tindaklanjut = $rowDua['tindaklanjut'];
-                                $target_waktu = $rowDua['target_waktu'];
-                                $indikator = $rowDua['indikator'];
-                                $namaindikator = $rowDua['namaindikator'];
-                                $rekomendasi = $rowDua['rekomendasi'];
-                                $namaopd = $rowDua['namaopd'];
-                                $nama_pendek_opd = $rowDua['nama_pendek_opd'];
-                                $namalevel = $rowDua['namalevel'];
-                                ?>
-
-                              <tr>
-                                <td><?= $no; ?></td>
-                                <td><?= $tahun_penilaian; ?></td>
-                                <td><?= $tahun_penilaian; ?></td>
-                                <td><?= $namalevel; ?></td>
-                                <td><?= $namalevel; ?></td>
-                                <td><?= $namalevel; ?></td>
-                              </tr>
-                          <?php
-                                $no++;
-                              }
-                            }
-                            ?>
-                        </tbody>
-                      </table>
+                  <div id="collapse<?= $var; ?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                      <div class="table-responsive">
+                        <table class="table table-bordered table-hover" id="" width="100%" cellspacing="0">
+                          <tbody>
+                            <tr>
+                              <th>Nilai Pusat</th>
+                              <td><?= $nilai_pusat; ?></td>
+                            </tr>
+                            <tr>
+                              <th>Tahap Yang Harus dipenuhi OPD</th>
+                              <td>ASD</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            <?php $var++; } ?>
+            <?php $var++; } 
+              } else { echo "<h5>Data Belum tersedia</h5>"; } ?>
             </div>
           </div>
         </div>
