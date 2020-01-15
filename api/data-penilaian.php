4175108f0c0c30c8b713708b3f56b1d3f2b730ce
Okky Maheswara <okkymahes@gmail.com>
2020-01-15 10:32:30 +0800

    membuat query

--- api/data-penilaian.php
+++ api/data-penilaian.php
@@ -3,10 +3,9 @@ include '../config/config.php';
 
 $id = $_GET['id'];
 
-$sql = "SELECT * FROM tb_level a 
-        LEFT JOIN tb_pertanyaan b ON a.idpertanyaan = b.idpertanyaan
-        LEFT JOIN tb_indikator c ON b.idindikator = c.idindikator 
-        WHERE b.idindikator = $id";
+$sql = "SELECT * FROM tb_penilaian a 
+        LEFT JOIN tb_indikator b ON b.idindikator = a.idindikator 
+        WHERE a.tahun_penilaian = $id";
 $result = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_assoc($result)) {
