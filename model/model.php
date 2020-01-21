1d9e9766afaa5daedd31312933ab2f621e2cb11a
Okky Maheswara <okkymahes@gmail.com>
2020-01-21 11:35:01 +0800

    upload upload

--- model/model.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include 'model_indikator.php';
-include 'model_aspek.php';
-include 'model_domain.php';
--- model/model_domain.php
+++ model/model_domain.php
@@ -3,8 +3,11 @@ class model_domain
 {
     function all()
     {
-        echo 'Monev SPBE';
+        $sqlDomain = "SELECT * FROM tb_domain a 
+                    LEFT JOIN tb_indeks b ON b.id_indeks = a.id_indeks 
+                    WHERE b.nama_indeks = 'SPBE'
+                    ORDER BY a.urutan_domain ASC";
+        $resultDomain = mysqli_query($conn, $sqlDomain);
+        return $resultDomain;
     }
-
-    
 }
